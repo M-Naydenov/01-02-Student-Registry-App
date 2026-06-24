f9d81a56ab6d522d7f04e034fd3ca3a1de02be60
M-Naydenov <mnaydenov.dev@gmail.com>
2026-06-24 10:24:12 +0300

    testing now for jenkins autotrigger on commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(require('body-parser')
   .urlencoded({extended:true}));
-//comment test
+//comment test for jenkins trigger
 const studentsController = 
   require("./controllers/students-controller");
 
